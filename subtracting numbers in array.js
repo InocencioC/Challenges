b4a7dc02280349061599fb062b000e87485c20e7
Inocencio Cordeiro <34503843+InocencioC@users.noreply.github.com>
2022-06-17 14:56:40 +0100

    Details about the challenge
    I only add the comments.

--- subtracting numbers in array.js
+++ subtracting numbers in array.js
@@ -1,7 +1,16 @@
+////
+Desafio fatorial *
+Considerando que fatoriais são o resultado da multiplicação de todos os números até certo número, ex: 3! = 3x2x1, resolva:
+1. Crie um array contendo todos os valores de fatoriais de 0 até 9; 
+2. Considerando o valor de 500 mil como inicial, retorne o valor final após subtrair o valor inicial por cada valor dentro do array.
+/////
+
 function main() {
     let array = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
     const resultado = array.map(element => 500000 - element)
     console.log(resultado);
 }
  main()
+
+
     
