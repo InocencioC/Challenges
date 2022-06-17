520def23ac738b1cb594c977fadbfc14297aa163
InocencioC <cordeiroturorias@gmail.com>
2022-06-17 14:53:17 +0100

    first commit

---
+++ subtracting numbers in array.js
@@ -0,0 +1,7 @@
+function main() {
+    let array = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+    const resultado = array.map(element => 500000 - element)
+    console.log(resultado);
+}
+ main()
+    
